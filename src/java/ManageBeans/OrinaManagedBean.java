4eda4276af1c1b93c195d525f776998200abe0bd
Alex Jimenez <daikenkaist@gmail.com>
2015-09-07 00:12:30 -0600

    Creacion managebean para examen de orina

---
+++ src/java/ManageBeans/OrinaManagedBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ManageBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author MacAlex
+ */
+@ManagedBean
+@SessionScoped
+public class OrinaManagedBean {
+
+    /**
+     * Creates a new instance of OrinaManagedBean
+     */
+    public OrinaManagedBean() {
+    }
+    
+}
